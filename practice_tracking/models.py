3fad768ebb15a3b201dac03526642e990f8e9810
Scott Tolinski <scott.tolinski@gmail.com>
2012-11-02 00:02:51 -0400

    Added Move, MoveClass, Parent, UserProf to admin

--- practice_tracking/models.py
+++ practice_tracking/models.py
@@ -1,22 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# What type of move (footwork, combo, power...)
+class MoveClass(models.Model):
+	name = models.CharField(max_length=30)
+
+	def __unicode__(self):
+		return self.name
+
+class ParentMove(models.Model):
+	name = models.CharField(max_length=30)
+	move_classification = models.ForeignKey(MoveClass)
+
+	def __unicode__(self):
+		return self.name
+
 # Create your models here.
 class Move(models.Model):
 	name = models.CharField(max_length=60)
 	value = models.PositiveIntegerField()
-	parent = models.ForeignKey('ParentMove')
-	move_classification = models.ForeignKey('MoveClass')
-
-class ParentMove(models.Model):
-	name = models.CharField(max_length=30)
-	move_classification = models.ForeignKey('MoveClass')
-
+	parent = models.ManyToManyField(ParentMove)
+	move_classification = models.ForeignKey(MoveClass)
+ 
+	def __unicode__(self):
+		return self.name
 
 class Sesh(models.Model):
 	date = models.DateField()
-	moves = models.ManyToManyField('Move')
+	moves = models.ManyToManyField(Move)
 
+	def __unicode__(self):
+		return self.name
 
 class UserProfile(models.Model):
 	#connects to user
@@ -24,11 +38,10 @@ class UserProfile(models.Model):
 
 	#extra fields for user
 	value = models.PositiveIntegerField(default=0)
-	moves = models.ManyToManyField('Move')
+	moves = models.ManyToManyField(Move)
 	bboyname = models.CharField(max_length=30)
 	crew = models.CharField(max_length=30)
-
-# What type of move (footwork, combo, power...)
-class MoveClass(models.Model):
-	name = models.CharField(max_length=30)
+    
+	def __unicode__(self):
+		return self.name
     
